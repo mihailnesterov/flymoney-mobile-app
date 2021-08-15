dea518755a9edf3cfed1535e03245bde1c25b910
mihailnesterov <mhause@mail.ru>
2021-08-16 05:44:01 +0700

    add: api

--- App.js
+++ App.js
@@ -1,14 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { 
+    StyleSheet, 
+    Text, 
+    ScrollView,
+    View, 
+    Button, 
+    TextInput, 
+    FlatList, 
+    ActivityIndicator 
+} from 'react-native';
+import { getAllProducts } from './api/products';
+import { getAllFlymoneyExchange } from './api/posts';
+
 
 export default function App() {
-  return (
+
+    const [exchangeRates, setExchangeRates] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getAllFlymoneyExchange(100)
+            .then((response) => response.json())
+            .then((json) => setExchangeRates(json))
+            .catch((error) => {
+                console.error(error);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    useEffect(() => {
+        getAllProducts()
+            .then((data) => setProducts(data))
+            .catch(error => {
+                console.log(error);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    console.log(products, exchangeRates);
+
+return (
     <View style={styles.container}>
-      <Text>App created!</Text>
-      <StatusBar style="auto" />
+        <Text>Банки:</Text>
+        {isLoading ? <ActivityIndicator/> : (
+            <FlatList
+                style={{padding:10}}
+                data={products}
+                keyExtractor={({ id }, index) => id}
+                renderItem={({ item }) => (
+                    <Text style={{fontSize:16, marginBottom:10}}>{item.id}  {item.name} {item.variations.length}</Text>
+                )}
+            />
+        )}
+        <Text>Курсы валют:</Text>
+        {isLoading ? <ActivityIndicator/> : (
+            <FlatList
+                style={{padding:10}}
+                data={exchangeRates}
+                keyExtractor={({ id }, index) => id}
+                renderItem={({ item }) => (
+                    <Text style={{fontSize:16, marginBottom:10}}>
+                        {item.id} {item.title.rendered}  {item.meta.flymoney_exchange_meta_base_name}
+                    </Text>
+                )}
+            />
+        )}
+        <StatusBar style="auto" />
     </View>
-  );
+);
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +78,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 60
   },
 });
