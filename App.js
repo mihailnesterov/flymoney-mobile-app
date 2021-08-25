98a368612e0b51f9481e1191a787c7c36f7a086f
mihailnesterov <mhause@mail.ru>
2021-08-26 02:00:00 +0700

    add: tabbar navigation

--- App.js
+++ App.js
@@ -4,17 +4,7 @@ import { LogBox } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './screens/Home';
-import Order from './screens/Order';
-import Account from './screens/Account';
-import Auth from './screens/Auth';
-import Agreement from './screens/Agreement';
-import Faq from './screens/Faq';
-import Safety from './screens/Safety';
-import Howto from './screens/Howto';
-
-const Tab = createBottomTabNavigator();
+import BottomTabNavigator from './navigation/BottomTabNavigator';
 
 export default App = () => {
 
@@ -25,19 +15,7 @@ export default App = () => {
     return (
         <Provider store={store}>
             <NavigationContainer>
-                <Tab.Navigator 
-                    initialRouteName="Home" 
-                    screenOptions={{ headerShown: false }}
-                >
-                    <Tab.Screen name="Home" component={Home} />
-                    <Tab.Screen name="Order" component={Order} />
-                    <Tab.Screen name="Auth" component={Auth} />
-                    <Tab.Screen name="Account" component={Account} />
-                    <Tab.Screen name="Agreement" component={Agreement} />
-                    <Tab.Screen name="Faq" component={Faq} />
-                    <Tab.Screen name="Safety" component={Safety} />
-                    <Tab.Screen name="Howto" component={Howto} />
-                </Tab.Navigator>
+                <BottomTabNavigator />
             </NavigationContainer>
             <StatusBar style="auto" />
         </Provider>
---
+++ navigation/BottomTabNavigator.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, Octicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import Home from '../screens/Home';
+import Account from '../screens/Account';
+import Faq from '../screens/Faq';
+import Howto from '../screens/Howto';
+
+const Tab = createBottomTabNavigator();
+
+const getTabBarIcons = (route, focused, color, size) => {
+    let iconName;
+
+    switch (route.name) {
+        case 'Home':
+            iconName = focused ? 'home' : 'home-outline';
+            break;
+        case 'Account':
+            iconName = focused ? 'person-circle' : 'person-circle-outline';
+            break;
+        case 'Agreement':
+            iconName = focused ? 'document-text' : 'document-text-outline';
+            break;
+        case 'Faq':
+            iconName = focused ? 'frequently-asked-questions' : 'frequently-asked-questions';
+            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+        case 'Howto':
+            iconName = focused ? 'question' : 'question';
+            return <Octicons name={iconName} size={size} color={color} />;
+    }
+
+    return <Ionicons name={iconName} size={size} color={color} />;
+}
+
+const TabScreen = (name, component, label) => {
+    return(
+        <Tab.Screen 
+            name = {name} 
+            component = {component} 
+            options = {{ tabBarLabel: label }} 
+        />
+    );
+}
+
+const TintColors = {
+    active: '#7854f7',
+    inactive: '#666666',
+}
+
+export default BottomTabNavigator = () => {
+    return(
+        <Tab.Navigator 
+            initialRouteName="Home" 
+            screenOptions={({ route }) => ({ 
+                headerShown: false,
+                tabBarActiveTintColor: TintColors.active,
+                tabBarInactiveTintColor: TintColors.inactive,
+                tabBarIcon: ({ 
+                    focused, 
+                    color, 
+                    size 
+                }) => getTabBarIcons(route, focused, color, size),
+            })}
+        >
+            { TabScreen("Home", Home, 'В начало') }
+            { TabScreen("Account", Account, 'Аккаунт') }
+            { TabScreen("Agreement", Account, 'Соглашение') }
+            { TabScreen("Faq", Faq, 'FAQ') }
+            { TabScreen("Howto", Howto, 'Как создать?') }
+           
+        </Tab.Navigator>
+    );
+}
